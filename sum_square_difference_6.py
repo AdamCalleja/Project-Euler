9ecc4592082b589bbbe4a9b41410f4cd1f986853
Adam Calleja <adam1.calleja@gmail.com>
2021-04-04 14:04:36 +0100

    Updated sum_square_difference_6.py
    Created first draft of solution to problem. I forgot that the correct operator for exponentation is ** (I though that the correct operator is ^). I quickly realised that I was using the wrong operator and fixed the error immediately

---
+++ sum_square_difference_6.py
@@ -0,0 +1,7 @@
+first_100_natural_numbers = [integer for integer in range(1, 101)]
+squares_of_first_100_natural_numbers = [interger ** 2 for integer in range(1, 101)]
+sum_of_first_100_natural_numbers = sum(first_100_natural_numbers)
+sum_of_squares = sum(squares_of_first_100_natural_numbers)
+square_of_sum = sum_of_first_100_natural_numbers ** 2
+
+difference = square_of_sum - sum_of_squares
