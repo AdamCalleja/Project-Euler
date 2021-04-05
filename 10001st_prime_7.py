95a33b6c2b282b20a1a03a702de2ffe32ffb3e47
Adam Calleja <adam1.calleja@gmail.com>
2021-04-05 08:48:32 +0100

    Update 10001st_prime_7.py
    Wrote first draft of attempt to solve the 7th Project Euler problem

---
+++ 10001st_prime_7.py
@@ -0,0 +1,15 @@
+import prime_number_test
+## Imported a module containing a function to test whether an input is prime. 
+
+prime_number_counter = 0
+iterator = 0 
+
+while prime_number_counter != 10001:
+    iterator += 1
+    if prime_number_test.is_prime(iterator) == True:
+        prime_number_counter += 1
+
+print(iterator)
+
+
+
