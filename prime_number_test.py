4f4d620c1ced89fa1fe69c871804d4f9ee3e2f82
Adam Calleja <adam1.calleja@gmail.com>
2021-04-05 08:39:32 +0100

    Update prime_number_test.py
    Modified function to output 'False' for an input of '1', since this is an unexpected result.

--- prime_number_test.py
+++ prime_number_test.py
@@ -1,4 +1,6 @@
 def is_prime(integer):
+    if integer == 1:
+        return False
     if isinstance(integer, int) == False:
         print(integer, "is not an integer. Please input an integer.")
     for potential_factor in range(2, integer):
