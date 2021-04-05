7c929a6581d75080dec5d7304d80567edec1afae
Adam Calleja <adam1.calleja@gmail.com>
2021-04-05 08:32:59 +0100

    Update prime_number_test.py
    Fixed an error in the is_prime() function. I originally had the function return True if the input is not a prime and False if the input is a prime, however the function should output the opposite. This error was easy to correct as I only needed to switch the 'True' and 'False' on the return statements.

--- prime_number_test.py
+++ prime_number_test.py
@@ -3,5 +3,5 @@ def is_prime(integer):
         print(integer, "is not an integer. Please input an integer.")
     for potential_factor in range(2, integer):
         if integer % potential_factor == 0:
-            return True
-    return False
+            return False
+    return True 
