17ddd78d4418701e3ea7ad0a52e115ffe31b021d
Adam Calleja <adam1.calleja@gmail.com>
2021-04-05 08:56:07 +0100

    Update 10001st_prime_7.py
    Increasing iterator by 2 instead of 1 to ignore all even numbers, since even numbers aren't prime. This will speed up the program

--- 10001st_prime_7.py
+++ 10001st_prime_7.py
@@ -2,10 +2,10 @@ import prime_number_test
 ## Imported a module containing a function to test whether an input is prime. 
 
 prime_number_counter = 0
-iterator = 0 
+iterator = 1 
 
 while prime_number_counter != 10001:
-    iterator += 1
+    iterator += 2
     print(iterator)
     if prime_number_test.is_prime(iterator) == True:
         prime_number_counter += 1
