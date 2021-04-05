f91cdbd51ab35ea56539c7c1e6275c0501d906fc
Adam Calleja <adam1.calleja@gmail.com>
2021-04-05 08:26:49 +0100

    Update prime_number_test,py
    Created initial draft of the function, is_prime(), that tests whether a given number is prime.

---
+++ prime_number_test.py
@@ -0,0 +1,7 @@
+def is_prime(integer):
+    if isinstance(integer, int) == False:
+        print(integer, "is not an integer. Please input an integer.")
+    for potential_factor in range(2, integer):
+        if integer % potential_factor == 0:
+            return True
+    return False
