a6ca82129852e4555dc67765afd5511b3b27a06c
Adam Calleja <adam1.calleja@gmail.com>
2021-04-08 18:06:33 +0100

    Update special_pythagorean_triplet_9.py
    I realised that I used the wrong variable name for the integer when I added the additional 'if' statement in the previous commit. I named the integer 'initial_integer' rather than just 'integer'. This was an easy mistake to fix since I just had to rename one variable.

--- special_pythagorean_triplet_9.py
+++ special_pythagorean_triplet_9.py
@@ -8,7 +8,7 @@ def pythagorean_triplet(integer):
     while isinstance((math.sqrt((integer ** 2) + (other_integer ** 2))), int) == False:
         other_integer += 1
         if (math.sqrt((integer ** 2) + (other_integer ** 2))) > (integer + other_integer):
-            initial_integer += 1
+            integer += 1
     return [integer, other_integer, (math.sqrt((integer ** 2) + (other_integer ** 2)))]
 
 while pythagorean_triplet_sum != 1000:
