49762913d4187cc855826b6c340efa2376db5f7b
Adam Calleja <adam1.calleja@gmail.com>
2021-04-09 20:54:01 +0100

    Update special_pythagorean_triplet_9.py
    Updated the 'pythagorean_triplet()' function with code that should fix the error that I was having. I have now added an if statement that exits the funciton if no integer triplets exist for that integer. I have also updated some variable names with names that I believe help keep the code clean.

--- special_pythagorean_triplet_9.py
+++ special_pythagorean_triplet_9.py
@@ -1,23 +1,25 @@
 import math
 
+desired_pythagorean_triplet_sum = 1000
 pythagorean_triplet_sum = 0
-initial_integer = 0
+pythagorean_integer_a = 0
 
-def pythagorean_triplet(integer):
-    other_integer = 1
-    while isinstance((math.sqrt((integer ** 2) + (other_integer ** 2))), int) == False:
-        other_integer += 1
-        if (math.sqrt((integer ** 2) + (other_integer ** 2))) > (integer + other_integer):
-            integer += 1
-    return [integer, other_integer, (math.sqrt((integer ** 2) + (other_integer ** 2)))]
+def pythagorean_triplet(integer_a):
+    integer_b = 1
+    hypotenuse = math.sqrt((integer_a ** 2) + (integer_b ** 2))
+    while isinstance(hypotenuse, int) == False:
+        integer_b += 1
+        if hypotenuse + integer_a + integer_b > desired_pythagorean_triplet_sum:
+            return []
+    return [integer, other_integer, hypotenuse]
 
-while pythagorean_triplet_sum != 1000:
-    initial_integer += 1
-    pythagorean_triplet_sum = sum(pythagorean_triplet(initial_integer))
-    print(sum(pythagorean_triplet(initial_integer)))
+while pythagorean_triplet_sum != desired_pythagorean_triplet_sum:
+    pythagorean_integer_a += 1
+    pythagorean_triplet_sum = sum(pythagorean_triplet(pythagorean_integer_a))
+    print(sum(pythagorean_triplet(pythagorean_integer_a)))
 
-integer_a = pythagorean_triplet(initial_integer)[0]
-integer_b = pythagorean_triplet(initial_integer)[1]
-integer_c = pythagorean_triplet(initial_integer)[2]
+integer_a = pythagorean_triplet(pythagorean_integer_a)[0]
+integer_b = pythagorean_triplet(pythagorean_integer_a)[1]
+integer_c = pythagorean_triplet(pythagorean_integer_a)[2]
 
 print(integer_a * integer_b * integer_c)
