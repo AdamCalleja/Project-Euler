65016bc99c788e3fc23c3267036f26c16257118d
Adam Calleja <adam1.calleja@gmail.com>
2021-04-10 12:26:28 +0100

    Update summation_of_primes_10.py
    My code was taking a very long time to run and I realised that I may have misunderstood the problem. I originally thought that I had to find the sum of all the primes which have values below 2 000 000, however I now believe that it is the sum that is meant to be below 2 000 000. I have modified my code for this new understanding of the problem.

--- summation_of_primes_10.py
+++ summation_of_primes_10.py
@@ -1,7 +1,25 @@
+#import prime_number_test
+#
+#prime_number_cap = 2000000
+#
+#prime_numbers = [integer for integer in range(1, (prime_number_cap + 1)) if prime_number_test.is_prime(integer) == True]
+#
+#print(sum(prime_numbers))
+
 import prime_number_test
 
-prime_number_cap = 2000000
+prime_sum_cap = 2000000
+loop_control = True
+prime_sum = 0
+prime_numbers = []
+initial_integer = 0
 
-prime_numbers = [integer for integer in range(1, (prime_number_cap + 1)) if prime_number_test.is_prime(integer) == True]
+while loop_control == True:
+    if prime_number_test.is_prime(initial_integer) == True:
+        prime_numbers.append(initial_integer)
+    initial_integer += 1
+    prime_sum = sum(prime_numbers)
+    if prime_sum > 2000000:
+        loop_control = False
 
-print(sum(prime_numbers))
+print(prime_sum)
