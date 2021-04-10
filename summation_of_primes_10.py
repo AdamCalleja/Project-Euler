bf01ce73dd58ca86c757884f450f865e5accd51e
Adam Calleja <adam1.calleja@gmail.com>
2021-04-10 12:36:51 +0100

    Update summation_of_primes_10.py
    It turns out that my original understanding of the problem was correct. After doing some research I discovered that my implementation of a prime number test is very slow and that this Project Euler problem is designed to help you realise that an implementation similar to my implementation is very inefficient. This problem is designed to make you research more efficient implementations. As a result, I am going to research how to implement a prime number test efficiently and then redo this problem.

--- summation_of_primes_10.py
+++ summation_of_primes_10.py
@@ -1,26 +1,7 @@
-#import prime_number_test
-#
-#prime_number_cap = 2000000
-#
-#prime_numbers = [integer for integer in range(1, (prime_number_cap + 1)) if prime_number_test.is_prime(integer) == True]
-#
-#print(sum(prime_numbers))
-
 import prime_number_test
 
-prime_sum_cap = 2000000
-prime_sum = 0
-prime_numbers = []
-initial_integer = 0
+prime_number_cap = 2000000
 
-while prime_sum < prime_sum_cap:
-    if prime_number_test.is_prime(initial_integer) == True:
-        prime_numbers.append(initial_integer)
-    initial_integer += 1
-    prime_sum = sum(prime_numbers)
-    if prime_sum > 2000000:
-        prime_numbers = prime_numbers[0: -1]
+prime_numbers = [integer for integer in range(1, (prime_number_cap + 1)) if prime_number_test.is_prime(integer) == True]
 
-prime_sum = sum(prime_numbers)
-
-print(prime_sum)
+print(sum(prime_numbers))
