96d1ad0e9ad9487db055488c8fd0c1a1afd6dbe7
Adam Calleja <adam1.calleja@gmail.com>
2021-04-13 10:02:03 +0100

    Update prime_number_test_v1.py
    I updated the function that I used to check if the input is an integer. When I was updating this funciton I realised that my function didn't stop if the input was not an integer so I had to edit this as well. This was easily fixed by adding the term 'return' on the 6th line.

--- prime_number_test_v1.py
+++ prime_number_test_v1.py
@@ -1,8 +1,9 @@
 def is_prime(integer):
     if integer <= 1:
         return False
-    if isinstance(integer, int) == False:
+    if float(integer).is_integer() == False:
         print(integer, "is not an integer. Please input an integer.")
+        return
     for potential_factor in range(2, integer):
         if integer % potential_factor == 0:
             return False
