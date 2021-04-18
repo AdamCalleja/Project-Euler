1ae33997124531fd8eaa5874ecaa0f9470d16ccd
Adam Calleja <adam1.calleja@gmail.com>
2021-04-18 18:48:53 +0100

    Update sieve_of_eratosthenes.py
    I edited the sieve of eratosthenes module, putting all of the code within a 'main' function so that I can easily call the funciton from within a different module. I also removed some lines of code that I used to test if the module was working correctly.

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,56 +1,54 @@
-primes = []
-limit_value = 100000
-composite_indices = []
-total_popped = 0
+def main(limit_value):
+    primes = []
+    composite_indices = []
+    total_popped = 0
 
-# the 'limit_value' variable above holds the highest number that you
-# want to check. 
+    # the 'limit_value' variable above holds the highest number that you
+    # want to check. 
 
-highest_known_prime = 2
+    highest_known_prime = 2
 
-# This varible highest_known_prime will be used to determine all of 
-# the primes within the given range. 
+    # This varible highest_known_prime will be used to determine all of 
+    # the primes within the given range. 
 
-primes = [True for integer in range(2, limit_value + 1)]
+    primes = [True for integer in range(2, limit_value + 1)]
 
-# The above creates a list 'primes' holding the value 'True' for each
-# integer from 1 to the limit value. The integer itself can be determined
-# from the indieces of the elements. 
+    # The above creates a list 'primes' holding the value 'True' for each
+    # integer from 1 to the limit value. The integer itself can be determined
+    # from the indieces of the elements. 
 
-unchecked_integers = primes[(highest_known_prime - 2): (limit_value + 1)]
-next_multiple_index = (highest_known_prime + highest_known_prime - 2)
-
-## I have declared the varaible above in order to make my code more clean. 
-
-while unchecked_integers.count(False) < len(unchecked_integers):
-    if primes[(highest_known_prime - 2)] == True:
-        for multiple in range(next_multiple_index, (limit_value - 1), highest_known_prime):
-            primes[multiple] = False
-
-        # The above goes through all of the multiples of the highest_known_prime
-        # setting the corresponding value in 'primes' to false (only if
-        # highest_known_prime is not a multiple of a previous prime).
-        # note: the index 'multiple - 2' is used to account for the fact that 
-        #       indexing starts at 0 in Python. 
-
-    highest_known_prime += 1
     unchecked_integers = primes[(highest_known_prime - 2): (limit_value + 1)]
     next_multiple_index = (highest_known_prime + highest_known_prime - 2)
 
-for index in range(0, (limit_value - 1)):
-    if primes[index] == True:
-        primes[index] = index + 2
-    else:
-        composite_indices.append(index)
+    ## I have declared the varaible above in order to make my code more clean. 
 
-for index in composite_indices:
-    primes.pop(index - total_popped)
-    total_popped += 1
+    while unchecked_integers.count(False) < len(unchecked_integers):
+        if primes[(highest_known_prime - 2)] == True:
+            for multiple in range(next_multiple_index, (limit_value - 1), highest_known_prime):
+                primes[multiple] = False
 
-# The above replaces the elements with the value 'True' with the value
-# of the corresponding prime number in the list
-# of primes and deletes all of the elements with the value 'False'. 
+            # The above goes through all of the multiples of the highest_known_prime
+            # setting the corresponding value in 'primes' to false (only if
+            # highest_known_prime is not a multiple of a previous prime).
+            # note: the index 'multiple - 2' is used to account for the fact that 
+            #       indexing starts at 0 in Python. 
 
-prime_count = len(primes)
+        highest_known_prime += 1
+        unchecked_integers = primes[(highest_known_prime - 2): (limit_value + 1)]
+        next_multiple_index = (highest_known_prime + highest_known_prime - 2)
 
-print("There are", prime_count, "primes in the given range")
+    for index in range(0, (limit_value - 1)):
+        if primes[index] == True:
+            primes[index] = index + 2
+        else:
+            composite_indices.append(index)
+
+    for index in composite_indices:
+        primes.pop(index - total_popped)
+        total_popped += 1
+
+    # The above replaces the elements with the value 'True' with the value
+    # of the corresponding prime number in the list
+    # of primes and deletes all of the elements with the value 'False'. 
+
+    return primes
